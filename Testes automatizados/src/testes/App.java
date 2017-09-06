0e0180967001beb72fbb410dda59e81a04b59b54
am55053 <am55053@sep000524.saqueepague.local>
2017-09-06 20:13:38 -0300

    first commit

---
+++ Testes automatizados/src/testes/App.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package testes;
+
+/**
+ * @author am55053
+ *
+ */
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
