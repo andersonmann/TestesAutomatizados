023c131a03df042711205d32d2e392e9fa71af57
am55053 <am55053@sep000524.saqueepague.local>
2017-09-28 20:42:33 -0300

    adicionado mais testes

--- Testes automatizados/src/testesAutomatizados/Avaliador.java
+++ Testes automatizados/src/testesAutomatizados/Avaliador.java
@@ -3,6 +3,11 @@
  */
 package testesAutomatizados;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 /**
  * @author am55053
  *
@@ -10,6 +15,7 @@ package testesAutomatizados;
 public class Avaliador {
 	private double maiorDeTodos = Double.NEGATIVE_INFINITY;
 	private double menorDeTodos = Double.POSITIVE_INFINITY;
+	private List<Lance> maiores;
 
 	public void avalia(Leilao leilao) {
 		for (Lance lance : leilao.getLances()) {
@@ -19,8 +25,28 @@ public class Avaliador {
 			if (lance.getValor() < menorDeTodos) {
 				menorDeTodos = lance.getValor();
 			}
+			pegaOsMaioresNo(leilao);
 
 		}
+	}
+
+	private void pegaOsMaioresNo(Leilao leilao) {
+		maiores = new ArrayList<Lance>(leilao.getLances());
+		Collections.sort(maiores, new Comparator<Lance>() {
+			@Override
+			public int compare(Lance o1, Lance o2) {
+				if (o1.getValor() < o2.getValor())
+					return 1;
+				if (o1.getValor() > o2.getValor())
+					return -1;
+				return 0;
+			}
+		});
+		maiores = maiores.subList(0, maiores.size() > 3 ? 3 : maiores.size()); 
+	}
+
+	public List<Lance> getTresMaiores() {
+		return this.maiores;
 	}
 
 	public double getMaiorLance() {
--- Testes automatizados/src/testesAutomatizados/AvaliadorTest.java
+++ Testes automatizados/src/testesAutomatizados/AvaliadorTest.java
@@ -2,6 +2,8 @@ package testesAutomatizados;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.List;
+
 import org.junit.Test;
 
 public class AvaliadorTest {
@@ -61,6 +63,27 @@ public class AvaliadorTest {
 
 		assertEquals(1000, leiloeiro.getMaiorLance(), 0.0001);
 		assertEquals(1000, leiloeiro.getMenorLance(), 0.0001);
+	}
+
+	@Test
+	public void deveEncontrarOsTresMaioresLances() {
+		Usuario joao = new Usuario("João");
+		Usuario maria = new Usuario("Maria");
+		Leilao leilao = new Leilao("Playstation 3 Novo");
+
+		leilao.propoe(new Lance(joao, 100.0));
+		leilao.propoe(new Lance(maria, 200.0));
+		leilao.propoe(new Lance(joao, 300.0));
+		leilao.propoe(new Lance(maria, 400.0));
+
+		Avaliador leiloeiro = new Avaliador();
+		leiloeiro.avalia(leilao);
+
+		List<Lance> maiores = leiloeiro.getTresMaiores();
+		assertEquals(3, maiores.size());
+		assertEquals(400, maiores.get(0).getValor(), 0.00001);
+		assertEquals(300, maiores.get(1).getValor(), 0.00001);
+		assertEquals(200, maiores.get(2).getValor(), 0.00001);
 
 	}
 
